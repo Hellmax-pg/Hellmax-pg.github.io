03d3186a1878c08e8177866cd6f48a4f2f0c5a07
Maks <pegashev95@gmail.com>
2022-04-14 20:14:38 +0300

    fix bugs

--- src/components/bem-blocks/cart-modal/_cart-modal.js
+++ src/components/bem-blocks/cart-modal/_cart-modal.js
@@ -1,2 +1,6 @@
 document.querySelector('.header__cart-btn').addEventListener('click', () => openDialog(document.querySelector('.cart-modal')));
 document.querySelector('.cart-modal__close').addEventListener('click', () => closeDialog(document.querySelector('.cart-modal')));
+
+const productsBtn = document.querySelector('.product-card__btn');
+const cartProductsList = document.querySelector('.cart-modal__article');
+const cart = document.querySelector('.header__cart-btn');
