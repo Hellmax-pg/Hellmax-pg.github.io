d7674764ac7fa148bd524d2fee7047ce8ba1e6c4
Maks <pegashev95@gmail.com>
2022-04-27 01:44:16 +0300

    fix html and js in dialogs(2)

--- src/components/bem-blocks/_dialogs.js
+++ src/components/bem-blocks/_dialogs.js
@@ -16,7 +16,6 @@ function noScrollOff () {
 function openDialog(dialog) {
     noScroll();
     if (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) {
-        dialog.querySelector('.overlay').style.display = "none";
         dialog.showModal();
         dialog.style.opacity = 1;
     }
