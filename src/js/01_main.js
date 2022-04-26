2e4dd3ade57e8c801920ad437008f49a019be1bf
Maks <pegashev95@gmail.com>
2022-04-26 23:19:00 +0300

    add adaptive Tablet and laptop

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,3 +1,64 @@
+const mediaQueryMobile = window.matchMedia('(min-width:1px) and (max-width: 766px)');
+const mediaQueryTablet = window.matchMedia('(min-width:766px) and (max-width: 1366px)');
+const mediaQueryDesktop = window.matchMedia('(min-width:1366px) and (max-width: 2540px)');
+
+function handleTabletChangeMobile(e) {
+  if (e.matches) {
+    const botSlider = new Swiper('.swiper.bot-slider__slider', {
+      loop: true,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      pagination: {
+        el: ".bot-slider__slider .swiper-pagination",
+        clickable: true,
+        hashNavigation: true,
+      },
+      navigation: {
+        nextEl: '.bot-slider__slider .swiper-button-next',
+        prevEl: '.bot-slider__slider .swiper-button-prev',
+      },
+  });
+  }
+};
+
+function handleTabletChangeTablet(e) {
+  if (e.matches) {
+    const botSlider = new Swiper('.swiper.bot-slider__slider', {
+      loop: true,
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      pagination: {
+        el: ".bot-slider__slider .swiper-pagination",
+        clickable: true,
+        hashNavigation: true,
+      },
+      navigation: {
+        nextEl: '.bot-slider__slider .swiper-button-next',
+        prevEl: '.bot-slider__slider .swiper-button-prev',
+      },
+  });
+  }
+};
+
+function handleTabletChangeDesktop(e) {
+  if (e.matches) {
+    const botSlider = new Swiper('.swiper.bot-slider__slider', {
+      loop: true,
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+      pagination: {
+        el: ".bot-slider__slider .swiper-pagination",
+        clickable: true,
+        hashNavigation: true,
+      },
+      navigation: {
+        nextEl: '.bot-slider__slider .swiper-button-next',
+        prevEl: '.bot-slider__slider .swiper-button-prev',
+      },
+  });
+  }
+};
+
 window.addEventListener('scroll', function() {
     let $header = document.querySelector('.header'),
     top = pageYOffset;
@@ -10,7 +71,7 @@ window.addEventListener('scroll', function() {
 
 const topSlider = new Swiper('.swiper.top-slider__slider', {
     autoplay: {
-      delay: 5000,
+      delay: 3000,
     },
     loop: true,
     slidesPerView: 1,
@@ -37,22 +98,9 @@ const topSlider = new Swiper('.swiper.top-slider__slider', {
     }
 });
 
-const botSlider = new Swiper('.swiper.bot-slider__slider', {
-    autoplay: {
-      delay: 5000,
-    },
-    loop: true,
-    slidesPerView: 3,
-    slidesPerGroup: 3,
-    spaceBetween: 32,
-    pagination: {
-      el: ".bot-slider__slider .swiper-pagination",
-      clickable: true,
-      hashNavigation: true,
-    },
-    navigation: {
-      nextEl: '.bot-slider__slider .swiper-button-next',
-      prevEl: '.bot-slider__slider .swiper-button-prev',
-    },
-});
-
+mediaQueryTablet.addListener(handleTabletChangeTablet)
+handleTabletChangeTablet(mediaQueryTablet)
+mediaQueryDesktop.addListener(handleTabletChangeDesktop)
+handleTabletChangeDesktop(mediaQueryDesktop)
+mediaQueryMobile.addListener(handleTabletChangeMobile)
+handleTabletChangeMobile(mediaQueryMobile)
