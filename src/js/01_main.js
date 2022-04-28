45be0bf27a4cc2f12bab455d79044e8f7e191371
Maks <pegashev95@gmail.com>
2022-04-28 21:03:17 +0300

    adapted modals, fix bugs

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -24,20 +24,20 @@ function handleTabletChangeMobile(e) {
 
 function handleTabletChangeTablet(e) {
   if (e.matches) {
-    const botSlider = new Swiper('.swiper.bot-slider__slider', {
-      loop: true,
-      slidesPerView: 2,
-      slidesPerGroup: 1,
-      pagination: {
-        el: ".bot-slider__slider .swiper-pagination",
-        clickable: true,
-        hashNavigation: true,
-      },
-      navigation: {
-        nextEl: '.bot-slider__slider .swiper-button-next',
-        prevEl: '.bot-slider__slider .swiper-button-prev',
-      },
-  });
+      const botSlider = new Swiper('.swiper.bot-slider__slider', {
+        loop: true,
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        pagination: {
+          el: ".bot-slider__slider .swiper-pagination",
+          clickable: true,
+          hashNavigation: true,
+        },
+        navigation: {
+          nextEl: '.bot-slider__slider .swiper-button-next',
+          prevEl: '.bot-slider__slider .swiper-button-prev',
+        },
+    });
   }
 };
 
@@ -60,52 +60,59 @@ function handleTabletChangeDesktop(e) {
   }
 };
 
+mediaQueryTablet.addListener(handleTabletChangeTablet)
+handleTabletChangeTablet(mediaQueryTablet)
+mediaQueryDesktop.addListener(handleTabletChangeDesktop)
+handleTabletChangeDesktop(mediaQueryDesktop)
+mediaQueryMobile.addListener(handleTabletChangeMobile)
+handleTabletChangeMobile(mediaQueryMobile)
+
 window.addEventListener('scroll', function() {
-    let $header = document.querySelector('.header'),
-    top = pageYOffset;
-    if (top > 0) {
-        $header.classList.add('--scroll');
-    } else {
-        $header.classList.remove('--scroll');
-    }
+  let $header = document.querySelector('.header'),
+  top = pageYOffset;
+  if (top > 0) {
+      $header.classList.add('--scroll');
+  } else {
+      $header.classList.remove('--scroll');
+  }
 });
 
 const topSlider = new Swiper('.swiper.top-slider__slider', {
-    autoplay: {
-      delay: 3000,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: ".top-slider__slider .swiper-pagination",
+    clickable: true,
+    hashNavigation: true,
+    renderBullet: function (index, className) {
+      let productname = document.querySelectorAll('.swiper-slide')[index+1].dataset.productname
+      return `<span class="${className}">${productname}</span>`;
     },
-    loop: true,
-    slidesPerView: 1,
-    pagination: {
-      el: ".top-slider__slider .swiper-pagination",
-      clickable: true,
-      hashNavigation: true,
-      renderBullet: function (index, className) {
-        let productname = document.querySelectorAll('.swiper-slide')[index+1].dataset.productname
-        return `<span class="${className}">${productname}</span>`;
-      },
-    },
-    navigation: {
-      nextEl: '.top-slider__slider .swiper-button-next',
-      prevEl: '.top-slider__slider .swiper-button-prev',
-    },
-    on: {
-      slideChangeTransitionEnd: function() {
-        let modif = document.querySelector('.swiper-slide-active').dataset.modif,
-        topSliderClasses = document.querySelector('.top-slider').classList;
-        topSliderClasses.remove(topSliderClasses[1]);
-        topSliderClasses.add(modif)
-      }
+  },
+  navigation: {
+    nextEl: '.top-slider__slider .swiper-button-next',
+    prevEl: '.top-slider__slider .swiper-button-prev',
+  },
+  on: {
+    slideChangeTransitionEnd: function() {
+      let modif = document.querySelector('.swiper-slide-active').dataset.modif,
+      topSliderClasses = document.querySelector('.top-slider').classList;
+      topSliderClasses.remove(topSliderClasses[1]);
+      topSliderClasses.add(modif)
     }
+  }
 });
 
 document.querySelector('.header__menu-btn').addEventListener('click', function() {
-  item = document.querySelector('.menu');
-  if (item.classList.contains('active')) {
-    item.classList.remove("active");
-    item.classList.add("disable");
-  } else {
-    item.classList.remove("disable");
-    item.classList.add("active");
-  }
+item = document.querySelector('.menu');
+if (item.classList.contains('active')) {
+  item.classList.remove("active");
+  item.classList.add("disable");
+} else {
+  item.classList.remove("disable");
+  item.classList.add("active");
+}
 });
